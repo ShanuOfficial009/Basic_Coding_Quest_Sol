a26cbaeb1d810fba5541379486cfefd050e756a8
Shanu kumar <93045419+ShanuOfficial009@users.noreply.github.com>
2022-08-23 13:40:08 +0530

    Add files via upload

---
+++ str_end.java
@@ -0,0 +1,16 @@
+//Given a string, return a new string made of 3 copies of the last 2 chars of the original string.
+// The string length will be at least 2.
+//        extraEnd("Hello") → "lololo"
+//        extraEnd("ab") → "ababab"
+//        extraEnd("Hi") → "HiHiHi"
+
+
+package Basic_Quest;
+
+public class str_end {
+    public String extraEnd(String str){
+    String result = str.substring(str.length()-2);
+    return result + result + result;
+    }
+
+}
